3d83c98b687c2ff17d564520578c8e6f1ca6e9ad
Pranav <pranavjoy@protonmail.com>
2020-04-28 09:24:08 +0200

    Changed port to 8888

--- main.go
+++ main.go
@@ -24,5 +24,5 @@ func handler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	fmt.Println("Server started")
 	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8888", nil))
 }
